f2392cd0a001447d97fa857ecb2499453d0107a1
BrianNixLaptop <briannormant@gmail.com>
2026-01-25 01:54:08 -0500

    firenvim: default resize and use neovim command

--- after/plugin/firenvim.lua
+++ after/plugin/firenvim.lua
@@ -3,4 +3,8 @@ if vim.g.started_by_firenvim == true then
 	vim.o.cmdheight = 0
 	vim.o.showtabline = 0
 	vim.o.signcolumn = 'auto:1'
+	vim.g.firenvim_config.localSettings['.*'] = { cmdline = 'neovim' }
+
+	if vim.opt.lines:get() < 10 then vim.opt.lines = 10 end
+	if vim.opt.columns:get() < 80 then vim.opt.colums = 80 end
 end
