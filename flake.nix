13c45d8d71b95fd4bd49470c81d3c5b10128a389
BrianNixLaptop <briannormant@gmail.com>
2026-01-24 23:12:52 -0500

    fzf: theme through environment

--- flake.nix
+++ flake.nix
@@ -114,6 +114,7 @@
 			environmentVariables = {
 				git = {
 					VSCODE_DIFF_NO_AUTO_INSTALL = "1";
+					FZF_DEFAULT_OPTS = "--color=bg+:#3c3836,bg:#32302f,spinner:#8ec07c,hl:#83a598 --color=fg:#bdae93,header:#83a598,info:#fabd2f,pointer:#8ec07c --color=marker:#8ec07c,fg+:#ebdbb2,prompt:#fabd2f,hl+:#83a598 --color=bg+:#3c3836,bg:#32302f,spinner:#8ec07c,hl:#83a598 --color=fg:#bdae93,header:#83a598,info:#fabd2f,pointer:#8ec07c --color=marker:#8ec07c,fg+:#ebdbb2,prompt:#fabd2f,hl+:#83a598 --color=bg+:#3c3836,bg:#32302f,spinner:#8ec07c,hl:#83a598 --color=fg:#bdae93,header:#83a598,info:#fabd2f,pointer:#8ec07c --color=marker:#8ec07c,fg+:#ebdbb2,prompt:#fabd2f,hl+:#83a598";
 				};
 			};
 		};
